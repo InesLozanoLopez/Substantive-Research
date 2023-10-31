2c0bdc748980b56297b7b731ebddd820bd2c63b9
Ines Lozano <ineslozano@MacBook-Pro-de-Ines.local>
2023-10-31 12:03:04 +0100

    feat: add App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { fetchDataApi } from './ApiServices';
+import { Isector} from './interfaces';
 
 function App() {
+  const [dataApi, setDataApi] = useState<Isector[]>([]);
 
+  useEffect(() => {
+    fetchDataApi()
+    .then((response: Isector[]) => {
+      const updatedData = response.map((interplay) => {
+        return{
+            ...interplay,
+            interactions : interplay.interactions + 1,
+        };
+      });
+      setDataApi(updatedData)
+    }). catch ((error) => {
+
+      console.log('error from App.tsx', error)
+    })
+  }, [])
+
+
+  
   return (
-    <></>
+    <>
+    {dataApi.map((sector) => (
+    <div key={sector.sector_id}>
+      Name: {sector.name}, Interactions: {sector.interactions}
+    </div>
+    ))}
+    </>
   );
 }
 
---
+++ my-app/src/interfaces.tsx
@@ -0,0 +1,5 @@
+export interface Isector {
+    name: string,
+    sector_id: number,
+    interactions: number,
+}
