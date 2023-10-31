b024d3de2d9197280a848ffa2d0caba812b3c60b
Ines Lozano <ineslozano@MacBook-Pro-de-Ines.local>
2023-10-31 16:04:03 +0100

    feat: add CSS style

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { BiSolidSortAlt } from 'react-icons/bi';
 import './App.css';
 import { fetchDataApi } from './ApiServices';
 import { Isector } from './interfaces';
 
 function App() {
   const [dataApi, setDataApi] = useState<Isector[]>([]);
+  const [ sortedBy, setSortedBy] = useState<string>('');
 
   useEffect(() => {
     fetchDataApi()
       .then((response: Isector[]) => {
         const updatedData = response.reduce((data, interplay) => {
           const existingSector = data.find((sector) => sector.name === interplay.name);
-          if(existingSector){
+          if (existingSector) {
             existingSector.interactions += 1;
-          }else{
-              data.push({...interplay, interactions: 1});
-      }return data;
-    }, [] as Isector[]);
+          } else {
+            data.push({ ...interplay, sector_id: Number(interplay.sector_id), interactions: 1 });
+          } return data;
+        }, [] as Isector[]);
         setDataApi(updatedData)
-        console.log('updatedData', updatedData)
 
       })
       .catch((error) => {
@@ -26,16 +27,46 @@ function App() {
       })
   }, [])
 
-
+  const handleSortBy = (property: keyof Isector) =>{
+    const sortedDataApi = [...dataApi]
+    if(sortedBy === property){
+      sortedDataApi.reverse();
+    }else{
+      sortedDataApi.sort((a,b) => (a[property] < b[property] ? -1: 1))
+    }
+    setSortedBy(property);
+    setDataApi(sortedDataApi)
+  }
 
   return (
-    <>
-      {dataApi.map((sector) => (
-        <div key={sector.sector_id}>
-          Name: {sector.name}, Interactions: {sector.interactions}
-        </div>
-      ))}
-    </>
+    <div className='tableContainer'>
+      <h1>Substrantive Research</h1>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => handleSortBy('sector_id')}>
+              Sector ID <BiSolidSortAlt/>
+            </th>
+            <th onClick={() => handleSortBy('name')}>
+              Sector <BiSolidSortAlt/>
+            </th>
+            <th onClick={() => handleSortBy('interactions')}>
+              Nº of Interactions <BiSolidSortAlt/>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataApi.map((item) =>
+            <tr key={item.sector_id}>
+              <td>{item.sector_id}</td>
+              <td>{item.name}</td>
+              <td>{item.interactions}</td>
+            </tr>
+          )}
+        </tbody>
+
+      </table>
+    </div>
   );
 }
 
