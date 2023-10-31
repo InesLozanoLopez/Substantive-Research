e13cef9ad373ba3b807373dcb4845224630e56b8
Ines Lozano <ineslozano@MacBook-Pro-de-Ines.local>
2023-10-31 11:30:11 +0100

    feat: add APIServices

---
+++ my-app/src/ApiServices.tsx
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+export const fetchDataApi = async () => {
+    try{
+   const response = await axios({
+        method: 'get',
+        url: 'https://substantive.pythonanywhere.com/',
+    })
+
+    if(response.data){
+    return response.data
+    }
+    console.log('Error from fetchDataApi', response.status);
+    throw new Error;
+    
+}catch(error){
+    console.log('Error from apiServices', error);
+    throw error 
+}  
+}
