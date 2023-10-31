798755aeee5bff9fb6dbea855c18c2ddd174a2c3
Ines Lozano <ineslozano@MacBook-Pro-de-Ines.local>
2023-10-31 12:31:57 +0100

    fix: copilling sectors

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,36 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { fetchDataApi } from './ApiServices';
-import { Isector} from './interfaces';
+import { Isector } from './interfaces';
 
 function App() {
   const [dataApi, setDataApi] = useState<Isector[]>([]);
 
   useEffect(() => {
     fetchDataApi()
-    .then((response: Isector[]) => {
-      const updatedData = response.map((interplay) => {
-        return{
-            ...interplay,
-            interactions : interplay.interactions + 1,
-        };
-      });
-      setDataApi(updatedData)
-    })
-    .catch ((error) => {
-      console.log('error from App.tsx', error)
-    })
+      .then((response: Isector[]) => {
+        const updatedData = response.reduce((data, interplay) => {
+          const existingSector = data.find((sector) => sector.name === interplay.name);
+          if(existingSector){
+            existingSector.interactions += 1;
+          }else{
+              data.push({...interplay, interactions: 1});
+      }return data;
+    }, [] as Isector[]);
+        setDataApi(updatedData)
+        console.log('updatedData', updatedData)
+
+      })
+      .catch((error) => {
+        console.log('error from App.tsx', error)
+      })
   }, [])
 
 
-  
+
   return (
     <>
-    {dataApi.map((sector) => (
-    <div key={sector.sector_id}>
-      Name: {sector.name}, Interactions: {sector.interactions}
-    </div>
-    ))}
+      {dataApi.map((sector) => (
+        <div key={sector.sector_id}>
+          Name: {sector.name}, Interactions: {sector.interactions}
+        </div>
+      ))}
     </>
   );
 }
