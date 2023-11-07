300e3f034c72c214d2509621512def3de3d47568
Ines Lozano <ineslozano@MacBook-Pro-de-Ines.local>
2023-11-07 13:59:19 +0000

    fix: generate random blue colours on the GraphView and sector correct item

--- my-app/src/components/graphicView.tsx
+++ my-app/src/components/graphicView.tsx
@@ -1,43 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import '../App.css';
 import { Isector } from '../interfaces';
 import 'chart.js/auto';
 import { Chart } from 'react-chartjs-2';
-
-const initionalColors = [
-  'rgb(173, 255, 186)',
-  'rgb(192, 250, 185)',
-  'rgb(188, 235, 184)',
-  'rgb(177, 250, 166)',
-  'rgb(185, 251, 201)',
-  'rgb(188, 255, 172)',
-  'rgb(168, 245, 185)',
-  'rgb(171, 255, 156)',
-  'rgb(180, 247, 187)',
-  'rgb(163, 255, 186)'
-]
+import { getRandomColors} from '../functions';
 
 
 const GraphicView = ({ dataApi, hoveredSector }: { dataApi: Isector[], hoveredSector: number | null }) => {
   const [backgroundColorArray, setBackgroundColorArray] = useState<string[]>([]);
+  const initionalColors = useMemo(() => getRandomColors(dataApi.length), [dataApi]);
 
 
   useEffect(() => {
-    setBackgroundColorArray(initionalColors);
-  }, [dataApi]);
-
-  useEffect(() => {
-    console.log(hoveredSector);
     const newColors = [...initionalColors];
 
     if (hoveredSector !== null) {
       const sectorIndex = dataApi.findIndex(sector => sector.sector_id === hoveredSector);
-      
-      console.log('sectorIndex', sectorIndex);
-      
+            
       if (dataApi[sectorIndex].sector_id === hoveredSector) {
-        console.log('dataApi[sectorIndex].sector_id', dataApi[sectorIndex].sector_id)
-        newColors[sectorIndex] = 'hsl(0, 70%, 80%)';
+        newColors[sectorIndex] = 'hsl(30, 50%, 50%)';
     }}
     setBackgroundColorArray(newColors)
   }, [hoveredSector, dataApi])
--- my-app/src/functions.tsx
+++ my-app/src/functions.tsx
@@ -1,16 +1,18 @@
-export const percentage = (total: number, partial: number) : number => {
-    if (total === 0){
+export const percentage = (total: number, partial: number): number => {
+    if (total === 0) {
         return 0
     } else {
         return Math.round((partial / total) * 100);
     }
 }
 
-export function getRandomColor() {
-  const h = Math.floor(Math.random() * 360); // Random hue (0-360)
-  const s = Math.floor(Math.random() * 30) + 70; // Random saturation (70-100)
-  const l = Math.floor(Math.random() * 30) + 70; // Random lightness (70-100)
-
-  const color = `hsl(${h}, ${s}%, ${l}%)`;
-  return color;
-  }
+export function getRandomColors(sectors : number) {
+    const colorsArray = [];
+    for (let i = 0; i< sectors; i++){
+    const h = Math.floor(Math.random() * 60 + 180);
+    const s = Math.floor(Math.random() * 30) + 70;
+    const l = Math.floor(Math.random() * 30) + 70;
+    colorsArray.push(`hsl(${h}, ${s}%, ${l}%)`);
+    }
+    return colorsArray;
+}
